339a93e5b2a146490d49bbbac63f5eb4839402e7
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-24 21:43:18 -0400

    change routes/users.js to routes/books.js, make changes in app.js to remain consistent

---
+++ routes/books.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const router = express.Router()
+const { Book } = require('../models') // import your Book model
+
+// GET /books - list all books
+router.get('/', async (req, res, next) => {
+  try {
+    const books = await Book.findAll()
+    res.render('index', { books }) // pass books to the view
+  } catch (error) {
+    next(error)
+  }
+})
+
+// GET /books/new - show form to create a new book
+router.get('/new', (req, res) => {
+  res.render('new-book', { book: {} })
+})
+
+// POST /books/new - create a new book in the database
+router.post('/new', async (req, res, next) => {
+  try {
+    const book = await Book.create(req.body)
+    res.redirect('/books')
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      res.render('new-book', { book: req.body, errors: error.errors })
+    } else {
+      next(error)
+    }
+  }
+})
+
+// GET /books/:id - show details/edit form for a book
+router.get('/:id', async (req, res, next) => {
+  try {
+    const book = await Book.findByPk(req.params.id)
+    if (book) {
+      res.render('update-book', { book })
+    } else {
+      const err = new Error('Book not found')
+      err.status = 404
+      throw err
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// POST /books/:id - update book info in the database
+router.post('/:id', async (req, res, next) => {
+  try {
+    const book = await Book.findByPk(req.params.id)
+    if (book) {
+      await book.update(req.body)
+      res.redirect('/books')
+    } else {
+      const err = new Error('Book not found')
+      err.status = 404
+      throw err
+    }
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      req.body.id = req.params.id // keep id for form rendering
+      res.render('update-book', { book: req.body, errors: error.errors })
+    } else {
+      next(error)
+    }
+  }
+})
+
+// POST /books/:id/delete - delete a book
+router.post('/:id/delete', async (req, res, next) => {
+  try {
+    const book = await Book.findByPk(req.params.id)
+    if (book) {
+      await book.destroy()
+      res.redirect('/books')
+    } else {
+      const err = new Error('Book not found')
+      err.status = 404
+      throw err
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
